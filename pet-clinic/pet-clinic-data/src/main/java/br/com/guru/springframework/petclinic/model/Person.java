327cf52add42b573114959f136adee390384a57d
LuisaGPurificacao <luisa.purificacao16@gmail.com>
2023-01-19 11:13:20 -0300

    adding base entity with Long id

--- pet-clinic/pet-clinic-data/src/main/java/br/com/guru/springframework/petclinic/model/Person.java
+++ pet-clinic/pet-clinic-data/src/main/java/br/com/guru/springframework/petclinic/model/Person.java
@@ -3,7 +3,7 @@ package br.com.guru.springframework.petclinic.model;
 import lombok.Data;
 
 @Data
-public class Person {
+public class Person extends BaseEntity {
     private String firstName;
     private String lastName;
 }
--- pet-clinic/pet-clinic-data/src/main/java/br/com/guru/springframework/petclinic/model/PetType.java
+++ pet-clinic/pet-clinic-data/src/main/java/br/com/guru/springframework/petclinic/model/PetType.java
@@ -3,6 +3,6 @@ package br.com.guru.springframework.petclinic.model;
 import lombok.Data;
 
 @Data
-public class PetType {
+public class PetType extends BaseEntity {
     private String name;
 }
